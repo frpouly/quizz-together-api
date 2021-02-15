2fdc86481cb6f5521f859bb129b28172d9c9a9f2
François Pouly <francoispouly@hotmail.fr>
2021-02-15 16:46:13 +0100

    First commit, added Spring initializer

---
+++ src/main/java/com/quizztogether/api/QuizzTogetherApiApplication.java
@@ -0,0 +1,13 @@
+package com.quizztogether.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzTogetherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzTogetherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizztogether/api/QuizzTogetherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizztogether.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzTogetherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
