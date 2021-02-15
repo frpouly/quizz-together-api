d116d8e4b01e811b117b09b9058b3388b60b41de
François Pouly <francoispouly@hotmail.fr>
2021-02-15 17:17:30 +0100

    First Firebase test

--- src/main/java/com/quizztogether/api/FBInitialize.java
+++ src/main/java/com/quizztogether/api/FBInitialize.java
@@ -1,2 +1,31 @@
-package com.quizztogether.api;public class FBInitialize {
+package com.quizztogether.api;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Service
+public class FBInitialize {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
